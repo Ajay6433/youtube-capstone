81f385c94a54b7ba5e9b5ead8812bf74a582e2fb
Ajay6433 <Ajaykhan6433@gmail.com>
2025-08-16 16:50:41 +0530

    Videos uploaded succeffully

---
+++ youtube-backend/insertVideos.js
@@ -0,0 +1,28 @@
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import Video from ".//src/models/Video.model.js";
+// import videoData from "./DummyVideos.js"; // dummy data
+
+// dotenv.config();
+
+// async function insertVideos() {
+//   try {
+//     // Connect to MongoDB
+//     await mongoose.connect(process.env.MONGODB_URI);
+//     console.log("Connected to MongoDB...");
+
+//     // Insert videos
+//     const result = await Video.insertMany(videoData);
+//     console.log(`Inserted ${result.length} videos successfully.`);
+
+//     // Close connection manually
+//     await mongoose.connection.close();
+//     console.log("MongoDB connection closed.");
+
+//   } catch (error) {
+//     console.error("Error inserting videos:", error);
+//     process.exit(1);
+//   }
+// }
+
+// insertVideos();
